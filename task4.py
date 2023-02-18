75b02763a01d712a6c281dce82661cdeab13b4dd
ViktarYakimuk <ViktarYakimuk>
2023-02-18 17:10:49 +0300

    задача4.

---
+++ task4.py
@@ -0,0 +1,24 @@
+# Реализуйте RLE алгоритм:
+#  реализуйте модуль сжатия и восстановления данных.
+
+def encryption(s):
+   encoding = ''
+   i = 0
+
+   while i < len(s):
+      count = 1
+
+      while i + 1 < len(s) and s[i] == s[i + 1]:
+            count = count + 1
+            i = i + 1
+
+      encoding += str(count) + s[i]
+      i = i + 1
+
+   return encoding
+
+
+if __name__ == '__main__':
+
+   s = input('Введите шифр - >')
+   print(encryption(s))     
